6bc415713e7c018680d5c2468f0f7815ba8d85f6
brenin35 <brenoca35@gmail.com>
2025-10-17 13:31:14 -0300

    feat: client

---
+++ codigo/src/lib/client/auth-client.ts
@@ -0,0 +1,8 @@
+import { adminClient } from 'better-auth/client/plugins';
+import { createAuthClient } from 'better-auth/svelte';
+
+export const authClient = createAuthClient({
+    plugins: [
+        adminClient()
+    ]
+});

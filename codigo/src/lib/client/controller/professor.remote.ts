381ca8b73da61a72b7ae3bfee9ec862055a29ba5
brenin35 <brenoca35@gmail.com>
2025-10-24 17:38:10 -0300

    fix: tipos

--- codigo/src/lib/client/controller/professor.remote.ts
+++ codigo/src/lib/client/controller/professor.remote.ts
@@ -4,61 +4,53 @@ import { z } from 'zod';
 import { professorModel } from '$lib/server/db/professor/model';
 import { alunoModel } from '$lib/server/db/aluno/model';
 import { transacaoModel } from '$lib/server/db/transacao/model';
-import type { SelectAluno } from '$lib/server/db/aluno/schema';
 
 export const listarProfessores = query(async () => {
-	
 	return await professorModel.listar();
 });
 
 export const listarAlunosParaTransferencia = query(async () => {
-	return (await alunoModel.listar()) as SelectAluno[];
+	return await alunoModel.listar()
 });
 
+export type AlunoWithUser = Awaited<ReturnType<typeof listarAlunosParaTransferencia>>[number];
+
 export const getProfessorData = query(
-	z.string().min(1, 'CPF é obrigatório'),
+	z.number().min(1, 'ID é obrigatório'),
 	async (cpf) => {
-		const data = await professorModel.buscarPorCpf(cpf);
+		const data = await professorModel.buscarPorId(cpf);
 		if (!data) throw new Error('Professor não encontrado');
 
-		return {
-			...data,
-			ultimaAtualizacao: new Date()
-		};
+		return data
 	}
 );
 
 
 export const getHistoricoProfessor = query(
-	z.string().min(1, 'CPF é obrigatório'),
+	z.number().min(1, 'CPF é obrigatório'),
 	async (cpf) => {
 		const transacoes = await transacaoModel.listarPorProfessor(cpf);
-		return transacoes.map((t) => ({
-			...t,
-			data: new Date(t.data) 
-		}));
+		return transacoes
 	}
 );
 
-
 export const enviarMoedas = command(
 	z.object({
-		professorCpf: z.string().min(1, 'CPF do professor é obrigatório'),
+		professorId: z.number().min(1, 'ID do professor é obrigatório'),
 		alunoId: z.number().min(1, 'Aluno é obrigatório'),
 		quantidade: z.number().min(1, 'Quantidade deve ser maior que 0'),
 		motivo: z.string().min(1, 'Motivo é obrigatório')
 	}),
 	async (data) => {
-		
+
 		await transacaoModel.realizarTransferencia({
-			professorCpf: data.professorCpf,
+			professorId: data.professorId,
 			alunoId: data.alunoId,
 			valor: data.quantidade,
 			motivo: data.motivo
 		});
 
-		
-		await getProfessorData(data.professorCpf).refresh();
-		await getHistoricoProfessor(data.professorCpf).refresh();
+
+		await listarAlunosParaTransferencia().refresh();
 	}
 );

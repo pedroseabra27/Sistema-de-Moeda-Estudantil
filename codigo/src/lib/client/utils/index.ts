51d6b6b84fb2c9a9ba3cf4227e84e70fee5968ce
brenin35 <brenoca35@gmail.com>
2025-10-24 17:38:31 -0300

    feat: format utils

---
+++ codigo/src/lib/client/utils/index.ts
@@ -0,0 +1,7 @@
+export function formatCPF(cpf: string) {
+    return cpf.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, '$1.$2.$3-$4');
+}
+
+export function formatCurrency(value: number) {
+    return value
+}
